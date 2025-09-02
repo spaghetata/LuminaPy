d4cebb0d5b636407e6db2a6eb42109d606b7e6a5
Spaghetata <137336021+spaghetata@users.noreply.github.com>
2025-09-02 14:59:58 +0200

    Updated luminapy.py

--- luminapy.py
+++ luminapy.py
@@ -4,20 +4,32 @@
 #################   Informations   #################
                 ####################
 
-Version     = "1.0.1"
+Version     = "1.1.0"
 Credits     = "spaghetata"
 License     = "GPL3.0"
-Discription = "This is a small script for highlighted error handling"
+Discription = "This is a small script for error handling"
+
+                ####################
+#################      Import      #################
+                ####################
+
+from datetime import datetime
+
+                ####################
+################# Global variables #################
+                ####################
+
+timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
                 ####################
 #################    Functions     #################
                 ####################
 
-def info(message):
-    print(f"\033[48;5;34m[INFO]\033[0m {message}")
+def info(text):
+    print(f"\033[48;5;34m[INFO]\033[0m {timestamp} - {text}") # green bg
 
-def warning(message):
-    print(f"\033[43m[WARNING]\033[0m {message}")
+def warning(text):
+    print(f"\033[43m[WARNING]\033[0m {timestamp} - {text}") # yellow bg
 
-def error(message):
-    print(f"\033[41m[ERROR]\033[0m {message}")
+def fail(text):
+    print(f"\033[41m[FAIL]\033[0m {timestamp} - {text}") # red bg
