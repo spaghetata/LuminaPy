088c33b16839d724244508c89d98288b13b187b8
Spaghetata <137336021+spaghetata@users.noreply.github.com>
2025-09-02 13:33:15 +0200

    luminapy.py aktualisiert

--- luminapy.py
+++ luminapy.py
@@ -4,7 +4,7 @@
 #################   Informations   #################
                 ####################
 
-Version     = "1.0.0"
+Version     = "1.0.1"
 Credits     = "spaghetata"
 License     = "GPL3.0"
 Discription = "This is a small script for highlighted error handling"
@@ -14,10 +14,10 @@ Discription = "This is a small script for highlighted error handling"
                 ####################
 
 def info(message):
-    print(f"\033[48;5;34mINFO:\033[0m {message}")
+    print(f"\033[48;5;34m[INFO]\033[0m {message}")
 
 def warning(message):
-    print(f"\033[43mWARNING:\033[0m {message}")
+    print(f"\033[43m[WARNING]\033[0m {message}")
 
 def error(message):
-    print(f"\033[41mERROR:\033[0m {message}")
+    print(f"\033[41m[ERROR]\033[0m {message}")
