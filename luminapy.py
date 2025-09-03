cf4f3bd2177441ebdde67fa3de1def38806da3e3
Spaghetata <137336021+spaghetata@users.noreply.github.com>
2025-09-03 12:27:39 +0200

    Updated luminapy.py

--- luminapy.py
+++ luminapy.py
@@ -4,7 +4,7 @@
 #################   Informations   #################
                 ####################
 
-Version     = "1.1.0"
+Version     = "1.1.1"
 Credits     = "spaghetata"
 License     = "GPL3.0"
 Discription = "This is a small script for error handling"
@@ -29,8 +29,9 @@ timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 def info(text):
     print(f"{BG.GREEN}{UTIL.BOLD}[INFO]{UTIL.RESET} {timestamp} - {text}") # green bg
 
-def warning(text):
-    print(f"{BG.YELLOW}{UTIL.BOLD}[WARNING]{UTIL.RESET} {timestamp} - {text}") # yellow bg
+def warn(text):
+    print(f"{BG.YELLOW}{UTIL.BOLD}[WARN]{UTIL.RESET} {timestamp} - {text}") # yellow bg
 
 def fail(text):
     print(f"{BG.RED}{UTIL.BOLD}[FAIL]{UTIL.RESET} {timestamp} - {text}") # red bg
+    exit(1)
