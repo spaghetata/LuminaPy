cad18083dbf9eee397f95cf2c61b0d96a5aa85a4
Spaghetata <137336021+spaghetata@users.noreply.github.com>
2026-02-05 10:28:33 +0100

    Updated to V1.2
    Changes:
    
    - fixed some issues with the timestamp

--- luminapy.py
+++ luminapy.py
@@ -4,7 +4,7 @@
 #################   Informations   #################
                 ####################
 
-Version     = "1.1.1"
+Version     = "1.2"
 Credits     = "spaghetata"
 License     = "GPL3.0"
 Discription = "This is a small script for error handling"
@@ -20,18 +20,16 @@ from ANSI import *
 ################# Global variables #################
                 ####################
 
-timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
                 ####################
 #################    Functions     #################
                 ####################
 
 def info(text):
-    print(f"{BG.GREEN}{UTIL.BOLD}[INFO]{UTIL.RESET} {timestamp} - {text}") # green bg
+    print(f"\n{BG.GREEN}{UTIL.BOLD}[INFO]{UTIL.RESET} {datetime.now().strftime("%Y-%m-%d %H:%M:%S")} - {text}") # green bg
 
 def warn(text):
-    print(f"{BG.YELLOW}{UTIL.BOLD}[WARN]{UTIL.RESET} {timestamp} - {text}") # yellow bg
+    print(f"\n{BG.YELLOW}{UTIL.BOLD}[WARN]{UTIL.RESET} {datetime.now().strftime("%Y-%m-%d %H:%M:%S")} - {text}") # yellow bg
 
 def fail(text):
-    print(f"{BG.RED}{UTIL.BOLD}[FAIL]{UTIL.RESET} {timestamp} - {text}") # red bg
+    print(f"\n{BG.RED}{UTIL.BOLD}[FAIL]{UTIL.RESET} {datetime.now().strftime("%Y-%m-%d %H:%M:%S")} - {text}") # red bg
     exit(1)
